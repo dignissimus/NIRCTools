3e7bd570df7b60d1848f04d00d92220e72957f5a
Sam Ezeh <sam@ezeh.me>
2017-09-04 22:27:02 +0100

    Added meaningful names to Strings.kt

--- NIRCTools/src/main/java/me/ezeh/nirct/tools/Strings.kt
+++ NIRCTools/src/main/java/me/ezeh/nirct/tools/Strings.kt
@@ -2,13 +2,13 @@ package me.ezeh.nirct.tools
 
 class Strings {
     companion object tools {
-        fun countChar(str: String, sub: Char): Int {
-            val subs = sub.toString()
-            val i = str.toCharArray()
+        fun countChar(str: String, toFind: Char): Int {
+            val toFindString = toFind.toString()
+            val count = str.toCharArray()
                     .asSequence()
                     .map { it.toString() }
-                    .count { it.equals(subs, ignoreCase = true) }
-            return i
+                    .count { it.equals(toFindString, ignoreCase = true) }
+            return count
         }
     }
 }
